dcaa668b1b3fda0e264041a8151796b6d0804658
Pratishtha Poudel <pratishtha.poudel@okstate.edu>
2017-10-17 10:06:09 -0500

    Added code to calculate GC content in a dna sequence.

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -1,4 +1,15 @@
 ## Analyze house elves' data on ear length and dna sequences.
 
 # Import data.
-houseelf_data <- read.csv("data/houseelf_earlength_dna_data.csv", header=TRUE)
+houseelf_data <- read.csv("data/houseelf_earlength_dna_data_1.csv", header=TRUE)
+
+# Calculates GC_content percentage irrespective of string case.
+
+library(stringr)
+
+gc_content <- function(sequence){
+  Gs <- str_count(sequence, regex('g', ignore_case = T))
+  Cs <- str_count(sequence, regex('c', ignore_case = T))
+  gc_content <- (Gs + Cs) / str_length(sequence) * 100
+  return(gc_content)
+}
