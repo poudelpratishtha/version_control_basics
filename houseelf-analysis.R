7a173574d6ffda22b3b4ca63736ce7c86d3b4653
Pratishtha Poudel <pratishtha.poudel@okstate.edu>
2017-10-17 09:41:56 -0500

    Added code to import the house-elves data.

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -1,1 +1,4 @@
-# Analyze house elves' data on ear length and dna sequences
+## Analyze house elves' data on ear length and dna sequences.
+
+# Import data.
+houseelf_data <- read.csv("data/houseelf_earlength_dna_data.csv", header=TRUE)
