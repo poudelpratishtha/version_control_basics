76836da548249a66ed01ba28da64342fb4b00447
Phillip D. Alderman <phillip.alderman@okstate.edu>
2017-10-19 10:10:44 -0500

    Adding a function to calculate the ear size class.

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -13,3 +13,9 @@ gc_content <- function(sequence){
   gc_content <- (Gs + Cs) / str_length(sequence) * 100
   return(gc_content)
 }
+
+get_size_class <- function(ear_length){
+   # Calculate the size class for one or more earth lengths
+   ear_lengths <- ifelse(ear_length > 10, "large", "small")
+   return(ear_lengths)
+}
